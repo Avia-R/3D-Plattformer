284b4b0e88e0e3d53cdcca5f90c8f83712b59559
Ailia Syed <ailia.syed02@gmail.com>
2022-01-18 23:52:39 +0100

    Player is ready so far

--- 3D Platformer/Assets/Imports/Player/PlayerMovement.cs
+++ 3D Platformer/Assets/Imports/Player/PlayerMovement.cs
@@ -10,38 +10,43 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runSpeed;
     private Vector3 moveDirection;
     private CharacterController controller;
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private float groundCheckDistance;
     private Vector3 velocity;
     [SerializeField] private float gravity;
-    [SerializeField] private LayerMask groundMask;
     [SerializeField] private float jumpHeight;
+
+    private Animator anim;
     // Start is called before the first frame update
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         Move();
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
     }
 
     private void Move() 
     {
-        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
-        Debug.Log(isGrounded);
 
-        if(isGrounded && velocity.y < 0)
+        if(controller.isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
         float moveZ = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal");
 
-        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = new Vector3(moveX, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
 
-        if(isGrounded)
+        if(controller.isGrounded)
         {
                 if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
             {
@@ -58,7 +63,7 @@ public class PlayerMovement : MonoBehaviour
             }
             moveDirection *= moveSpeed;
 
-            if(Input.GetKey(KeyCode.Space))
+            if(Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
             }
@@ -72,24 +77,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Idle()
     {
-
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
     }
 
     private void Walk()
     {
-        Debug.Log("Walk!");
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
         moveSpeed = walkSpeed;
     }
 
     private void Run()
     {
-        Debug.Log("Run!");
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
         moveSpeed = runSpeed;
     }
 
     private void Jump()
     {
-        Debug.Log("Jump!");
         velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("Attack");
+
     }
 }
